d4701f6dbe34c87f9fa1b772284df939ef75ff4d
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-11-20 03:37:28 +0530

    lint

--- stubsA123/BSTree.java
+++ stubsA123/BSTree.java
@@ -3,54 +3,44 @@
 
 public class BSTree extends Tree {
 
-    private BSTree left, right;     // Children.
-    private BSTree parent;          // Parent pointer.
-        
-    public BSTree(){  
+    private BSTree left, right; // Children.
+    private BSTree parent; // Parent pointer.
+
+    public BSTree() {
         super();
         // This acts as a sentinel root node
         // How to identify a sentinel node: A node with parent == null is SENTINEL NODE
         // The actual tree starts from one of the child of the sentinel node!.
-        // CONVENTION: Assume right child of the sentinel node holds the actual root! and left child will always be null.
-    }    
-
-    public BSTree(int address, int size, int key){
-        super(address, size, key); 
+        // CONVENTION: Assume right child of the sentinel node holds the actual root!
+        // and left child will always be null.
     }
 
-    public BSTree Insert(int address, int size, int key) 
-    { 
+    public BSTree(int address, int size, int key) {
+        super(address, size, key);
+    }
+
+    public BSTree Insert(int address, int size, int key) {
         return null;
     }
 
-    public boolean Delete(Dictionary e)
-    { 
+    public boolean Delete(Dictionary e) {
         return false;
     }
-        
-    public BSTree Find(int key, boolean exact)
-    { 
+
+    public BSTree Find(int key, boolean exact) {
         return null;
     }
 
-    public BSTree getFirst()
-    { 
+    public BSTree getFirst() {
         return null;
     }
 
-    public BSTree getNext()
-    { 
+    public BSTree getNext() {
         return null;
     }
 
-    public boolean sanity()
-    { 
+    public boolean sanity() {
         return false;
     }
 
 }
-
-
- 
-
-
