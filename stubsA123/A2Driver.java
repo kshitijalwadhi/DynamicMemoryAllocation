0c201ac9fc1d384f0f21045f2eec636e5ecdebcb
kshitijalwadhi <kshitijalwadhi@gmail.com>
2020-11-24 20:26:26 +0530

    new Driver + clean files

--- stubsA123/A2Driver.java
+++ stubsA123/A2Driver.java
@@ -5,10 +5,10 @@ import java.io.FileWriter;
 
 public class A2Driver {
     public static void main(String args[]) throws IOException {
-        int numTestCases;
-        File myObj = new File("./new_test.txt");
-        FileWriter fw = new FileWriter("./newoutput2.txt");
+        File myObj = new File("./test51.txt");
+        FileWriter fw = new FileWriter("./newoutput1.txt");
         Scanner sc = new Scanner(myObj);
+        int numTestCases;
         numTestCases = sc.nextInt();
         while (numTestCases-- > 0) {
             int size;
@@ -30,24 +30,19 @@ public class A2Driver {
                         result = obj.Free(argument);
                         break;
                     case "Defragment":
+                        argument = sc.nextInt();
                         obj.Defragment();
-                        result = -10;
                         break;
                     default:
                         break;
                 }
                 String str = String.valueOf(result);
-                if (result != -10) {
-                    System.out.println(str);
-                    fw.write(str);
-                    fw.write("\n");
-                } else {
-                    System.out.println("Defragmented");
-                    fw.write("Defragmented");
-                    fw.write("\n");
-                }
+                fw.write(str);
+                fw.write("\n");
             }
 
         }
+        fw.close();
+        sc.close();
     }
 }
